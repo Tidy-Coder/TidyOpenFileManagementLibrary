61d8456b7a673970b132877d6a13d97661a55b3f
TidyCoder <157531579+Tidy-Coder@users.noreply.github.com>
2024-08-03 16:42:03 +0100

    Update functions.c

--- c/functions.c
+++ c/functions.c
@@ -20,7 +20,7 @@ struct TidyResult{
 };
 typedef struct TidyResult TidyResult;
 
-# this function check if the file exists or don't exists.
+# this function check if the path is a directory.
 TidyResult tidyFg_isDirectory(const char[] tidyPath){
   TidyResult resultHere;
 #ifdef _WIN32
@@ -36,7 +36,6 @@ TidyResult tidyFg_isDirectory(const char[] tidyPath){
     resultHere.error = FALSE;
     resultHere.result = FALSE;
   }
-  return resultHere;
 #else
   struct stat tidyPathStat;
   if(stat(tidyPath, &tidyPathStat) != 0){
@@ -51,4 +50,37 @@ TidyResult tidyFg_isDirectory(const char[] tidyPath){
     resultHere.result = FALSE;
   }
 #endif
+  return resultHere;
+}
+# this function check if the path is a file.
+TidyResult tidyFg_isFile(const char[] tidyPath){
+  TidyResult resultHere;
+#ifdef _WIN32
+  DWORD tidyAttributes = getFileAttributes(tidyPath);
+  if(tidyAttributes == INVALIDE_FILE_ATTRIBUTES){
+    resultHere.error = TRUE;
+  }
+  if(tidyAttributes & FILE_ATTRIBUTE_DIRECTORY){
+    resultHere.error = FALSE;
+    resultHere.result = FALSE;
+  }
+  else{
+    resultHere.error = FALSE;
+    resultHere.result = TRUE;
+  }
+#else
+  struct stat tidyPathStat;
+  if(stat(tidyPath, &tidyPathStat) != 0){
+    resultHere.error = TRUE;
+  }
+  if(S_ISREG(tidyathStat.st_mode)){
+    resultHere.error = FALSE;
+    resultHere.result = TRUE;
+  }
+  else{
+    resultHere.error = FALSE;
+    resultHere.result = FALSE;
+  }
+#endif
+  return resultHere;
 }
