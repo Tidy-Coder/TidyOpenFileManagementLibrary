32284377e7fe872ee00b92044434c33799565bd3
TidyCoder <157531579+Tidy-Coder@users.noreply.github.com>
2024-08-03 16:36:28 +0100

    Update functions.c

--- c/functions.c
+++ c/functions.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/stat.h>
+#include <unistd.h>
+#endif
 
 enum TidyBoolean{
   FALSE = 0,
   TRUE = 1
 };
 typedef enum TidyBoolean TidyBoolean;
-struct TidyFileAndBoolean{
-  FILE* file,
-  char* typeOfFopen,
-  TidyBoolean error
-};
-typedef struct TidyFileAndBoolean TidyFileAndBoolean;
 
+struct TidyResult{
+  TidyBoolean result;
+  TidyBoolean error;
+};
+typedef struct TidyResult TidyResult;
 
 # this function check if the file exists or don't exists.
-TidyFileAndBoolean tidyFg_exists(char[] tidyFilename){
-  FILE* tidyFile = fopen(tidyFilename, "r")
-  TidyFileAndBoolean resultHere;
-  resultHere.file = tidyFile;
-  resultHere.typeOfFopen = "r";
-  if (tidyFile){
-    resultHere.error = FALSE;
-  }
-  else{
+TidyResult tidyFg_isDirectory(const char[] tidyPath){
+  TidyResult resultHere;
+#ifdef _WIN32
+  DWORD tidyAttributes = getFileAttributes(tidyPath);
+  if(tidyAttributes == INVALIDE_FILE_ATTRIBUTES){
     resultHere.error = TRUE;
   }
+  if(tidyAttributes & FILE_ATTRIBUTE_DIRECTORY){
+    resultHere.error = FALSE;
+    resultHere.result = TRUE;
+  }
+  else{
+    resultHere.error = FALSE;
+    resultHere.result = FALSE;
+  }
   return resultHere;
+#else
+  struct stat tidyPathStat;
+  if(stat(tidyPath, &tidyPathStat) != 0){
+    resultHere.error = TRUE;
+  }
+  if(S_ISDIR(tidyathStat.st_mode)){
+    resultHere.error = FALSE;
+    resultHere.result = TRUE;
+  }
+  else{
+    resultHere.error = FALSE;
+    resultHere.result = FALSE;
+  }
+#endif
 }
